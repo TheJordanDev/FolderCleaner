33d0e138012eaffd218284aaee86f0b0490a63b6
Jordan <thejordan.dev@gmail.com>
2024-12-01 01:57:16 +0100

    Ajout config pyinstaller

---
+++ build.bat
@@ -0,0 +1,9 @@
+python -m PyInstaller --noconfirm --onefile --console --icon "C:\Users\Jordan\Workspace\Python\FolderCleaner\icon.ico" --name "Folder Cleaner" --add-data "C:\Users\Jordan\Workspace\Python\FolderCleaner\config.py;." --add-data "C:\Users\Jordan\Workspace\Python\FolderCleaner\helper.py;." --add-data "C:\Users\Jordan\Workspace\Python\FolderCleaner\icon.png;." --add-data "C:\Users\Jordan\Workspace\Python\FolderCleaner\ui.py;." --add-data "C:\Users\Jordan\Workspace\Python\FolderCleaner\widgets.py;."  "C:\Users\Jordan\Workspace\Python\FolderCleaner\main.py"
+
+set /p deleteBuildFolder="Do you want to delete the build folder? (y/n): "
+if /i "%deleteBuildFolder%"=="y" (
+    rmdir /s /q build
+    echo Build folder deleted.
+) else (
+    echo Build folder not deleted.
+)
